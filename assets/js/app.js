8c46b18c7029940f50c517b1f0bb613e3bff646c
abdelhakhamdouni <abdelhakhamdouni@sfr.fr>
2022-01-17 19:24:07 +0100

    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,31 @@
+let textes = [
+    "CRÉATEUR & concépteur DE SITE WEB",
+    "CRÉATEUR d'application mobile",
+    "maintenace & mise à jour de site WEB",
+    "Audit SEO & référencement naturel",
+]
+
+let i = 0
+let title = document.querySelector('.site-title')
+
+let form = document.querySelector('form')
+let formElements = []
+
+/**
+ * On scroll
+ */
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY > 50){
+        document.querySelector('.navbar').classList.add('bg_dark')
+    }
+    else{
+        document.querySelector('.navbar').classList.remove('bg_dark')
+    }
+})
+
+/**
+ * Toggle menu
+ */
 if(document.querySelector('#menu_icon')){
     document.querySelector('#menu_icon').addEventListener('click', e => {
         e.target.classList.toggle('icon-indent-decrease')
@@ -9,49 +37,112 @@ if(document.querySelector('#menu_icon')){
     })
 }
 
-
-
-let textes = [
-    "CRÉATEUR & concépteur DE SITE WEB",
-    "CRÉATEUR d'application mobile",
-    "maintenace & mise à jour de site WEB",
-    "Audit SEO & référencement naturel",
-]
-
-
-let i = 0
-let title = document.querySelector('.site-title')
-
+/**
+ * Change site title
+ */
 setInterval(()=>{
     i++
     if(i==4) i = 0
     title.innerHTML = textes[i]
 }, 5000)
-if(document.querySelector('.range')){
-    document.querySelector('.range').classList.add('animate')
+
+/**
+ * Range animate
+ */
+if(document.querySelector('.range')){document.querySelector('.range').classList.add('animate')}
+
+/**
+ * Form Validate
+ */
+form.querySelectorAll('input').forEach( e => formElements.push(e) )
+form.querySelector('input[type="checkbox"').addEventListener('input', function(){
+    this.checked ? this.classList.add('valid') : this.classList.remove('valid')
+})
+form.querySelectorAll('input:not([type="checkbox"])').forEach( i => {
+    i.addEventListener('focus', e => {
+        let labelFor = e.target.getAttribute('name')
+        let label = document.querySelector(`label[for=${labelFor}]`)
+        label.classList.add('focusable')
+    })
+    i.addEventListener('input', function(e){
+        handler(e, 5)
+    })
+    i.addEventListener('blur', function(e){
+        handler(e, 5)
+    })
+})
+form.querySelectorAll('textarea').forEach( i => {
+    formElements.push(i)
+    i.addEventListener('focus', e => {
+        let labelFor = e.target.getAttribute('name')
+        let label = document.querySelector(`label[for=${labelFor}]`)
+        label.classList.add('focusable')
+    })
+    i.addEventListener('input', function(e){
+        handler(e, 50)
+    })
+    i.addEventListener('blur', function(e){
+        handler(e, 50)
+    })
+})
+function handler(event, number) {
+    let labelFor = event.target.getAttribute('name')
+    let label = document.querySelector(`label[for=${labelFor}]`)
+    if( event.target.value.length == 0){
+        label.classList.remove('focusable')
+        event.target.classList.remove('valid')
+        event.target.classList.remove('fail')
+    }
+    else if(event.target.value.length > number){
+        label.classList.add('success')
+        label.classList.remove('primary')
+        event.target.classList.add('valid')
+        event.target.classList.remove('fail')
+
+    }else{
+        label.classList.add('primary')
+        label.classList.remove('success')
+        event.target.classList.remove('valid')
+        event.target.classList.add('fail')
+    }
+    let valide = check_form()
+    console.log(valide)
+    valide ? form.querySelector('button').removeAttribute('disabled') : null
+}
+function check_form(){
+    let valide = true
+    formElements.forEach(e => {
+        if( ! e.classList.contains("valid")){
+            valide = false
+        }
+    })
+    return valide
 }
 
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY > 50){
-        document.querySelector('.navbar').classList.add('bg_dark')
-    }
-    else{
-        document.querySelector('.navbar').classList.remove('bg_dark')
-    }
+/**
+ * carousel testimonial
+ */
+
+let testi_list = document.querySelector('.testimonials_list')
+let steps = 0
+let direction = true;
+let testi_interval = setInterval(()=>{
+    steps++
+    if(steps == 5) steps = 0
+    testi_list.style.transform = `translateX(-${steps * 20}%)`
+    document.querySelectorAll('[id*="index_"').forEach(ele => ele.classList.remove('bg_primary'))
+    document.querySelector(`#index_${steps}`).classList.add('bg_primary')
+}, 5000)
+
+testi_list.addEventListener('mouseenter', e => {
+    clearInterval(testi_interval)
 })
-
-let nameInput = document.querySelector('input[name="name"')
-
-nameInput.addEventListener('blur', function(e){
-    if( this.value.length == 0){
-        this.classList.remove('valid')
-        this.classList.remove('fail')
-    }
-    else if(this.value.length > 5){
-        this.classList.add('valid')
-        this.classList.remove('fail')
-    }else{
-        this.classList.remove('valid')
-        this.classList.add('fail')
-    }
+testi_list.addEventListener('mouseleave', e => {
+    testi_interval = setInterval(()=>{
+        steps++ 
+        if(steps == 5) steps = 0
+        testi_list.style.transform = `translateX(-${steps * 20}%)`
+        document.querySelectorAll('[id*="index_"').forEach(ele => ele.classList.remove('bg_primary'))
+        document.querySelector(`#index_${steps}`).classList.add('bg_primary')
+    }, 5000)
 })
